716c9fc18d0e31063286ee3014f9613d155cfba5
Reji Shashi <reji.shashi@gmail.com>
2020-04-20 21:22:27 +0530

    change echo message

--- src/main/java/com/jcg/maven/App.java
+++ src/main/java/com/jcg/maven/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello SCM Poll World!" );
+        System.out.println( "Hello SCM Pollll World!" );
     }
 }
